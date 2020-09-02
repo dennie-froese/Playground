adc3072ab81da323e537c8c0391fb9fd32a7120e
Dennie Froese <dennie.froese@santander.co.uk>
2020-09-02 21:58:04 +0100

    formatting of sections page

--- src/pages/Sections/index.tsx
+++ src/pages/Sections/index.tsx
@@ -1,18 +1,45 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Pressable} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export default function Sections() {
   return (
-    <View>
-      <Text style={styles.text}>Sections</Text>
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollContainer}>
+        <Pressable style={styles.sectionContainer}>
+          <Text style={styles.header}>Header</Text>
+          <Text style={styles.main}>explanation</Text>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    textAlign: 'center',
-    paddingTop: 10,
+  container: {
+    flex: 1,
+  },
+  scrollContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+  },
+  sectionContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    margin: 20,
+    padding: 10,
+    backgroundColor: 'lightgrey',
+    borderRadius: 10,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 10,
+  },
+  main: {
+    fontSize: 15,
+    padding: 20,
   },
 });
