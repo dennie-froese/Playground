956449ae1b7dcf1c813fc4aee5105adeb62b89db
Dennie Froese <dennie.froese@santander.co.uk>
2020-09-02 17:11:32 +0100

    updated app.tsx to only hold home navigation stack

--- App.tsx
+++ App.tsx
@@ -1,31 +1,15 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, View, Text, Pressable} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeNavigator from './src/navigation/Home';
 
 export default function App() {
   return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.header}>Welcome to my Playground!</Text>
-        <Text style={styles.main}>
-          This is a place where I try out new things in the world of React /
-          React Native / Javascript.
-        </Text>
-        <Pressable style={styles.pressable} onPress={() => null}>
-          <Text>Go to different sections</Text>
-        </Pressable>
-      </View>
-    </>
+    <NavigationContainer>
+      <HomeNavigator />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    marginHorizontal: 20,
-  },
-  header: {fontSize: 20, fontWeight: 'bold'},
-  main: {fontSize: 15, textAlign: 'center'},
-  pressable: {backgroundColor: 'lightgrey', borderRadius: 8, padding: 6},
-});
+const styles = StyleSheet.create({});
